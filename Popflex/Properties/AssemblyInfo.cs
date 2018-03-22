d36b51340804db5aeb8818b31ee7130ac3370b9a
Oxymoron <timothysturm6@gmail.com>
2018-03-22 08:26:50 -0500

    boiler plate code & logos

---
+++ Popflex/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Popflex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Popflex")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f6928cc-a271-44ec-8ad7-53e8d01fd860")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Popflex: Purchase Order Printing from FlexOrders
-Natural, organic, non-GMO, and gluten free library that will take in a completed FlexOrder from the AllfleXML library, and convert it into a printable HTML document or PDF document by using [Pechkin](https://github.com/gmanny/Pechkin).
+
+<img src="https://raw.githubusercontent.com/Allflex/Popflex/master/Popflex.png" alt="Popflex Weasel" width="64" height="64" />
+
+Natural, organic, non-GMO, and gluten free library that will take in a completed FlexOrder from the AllfleXML library, and convert it into a printable HTML document or PDF document by using [Pechkin](https://github.com/gmanny/Pechkin) and [Handlebars.net](https://github.com/rexm/Handlebars.Net).
 
 
 <sup>Warning: May contain nuts.</sup>
