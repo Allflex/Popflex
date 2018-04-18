7520c0bb1043d2ca1d1d55c2d3c27481dcf7a268
Timothy Sturm <tsturm@allflexusa.com>
2018-04-18 12:48:36 -0500

    Fixed weird image fading issue

--- Popflex/OrderTemplate.cs
+++ Popflex/OrderTemplate.cs
@@ -5,7 +5,7 @@ namespace Popflex
     public class OrderTemplate : AllfleXML.FlexOrder.OrderHeader
     {
         public List<string> styles = new List<string>();
-        public string CompanyLogo = @"data:image/gif;base64," + System.Convert.ToBase64String(System.IO.File.ReadAllBytes(@"Resources\Images\DestronFearing.png"));
+        public string CompanyLogo = @"data:image/gif;base64," + System.Convert.ToBase64String(System.IO.File.ReadAllBytes(@"Resources\Images\Allflex.png"));
         public readonly string AllflexLogo = @"data:image/gif;base64," + System.Convert.ToBase64String(System.IO.File.ReadAllBytes(@"Resources\Images\Allflex.png"));
         public readonly string DestronLogo = @"data:image/gif;base64," + System.Convert.ToBase64String(System.IO.File.ReadAllBytes(@"Resources\Images\DestronFearing.png"));
     }
